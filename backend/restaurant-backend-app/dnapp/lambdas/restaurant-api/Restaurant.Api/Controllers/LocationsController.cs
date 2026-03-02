f6bdc84d786f6319382280ac3bf3419d223313d0
pidarinka <daryna.suprun@nure.ua>
2026-03-02 01:18:17 +0100

    Revert "update locations route"
    This reverts commit 5f8b29ee1321d5b9ccfca0349b0613985ab65801.

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Api/Controllers/LocationsController.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Api/Controllers/LocationsController.cs
@@ -7,7 +7,7 @@ using Restaurant.Core.Interfaces;
 namespace Restaurant.Api.Controllers
 {
     [ApiController]
-    [Route("locations")]
+    [Route("api/v1/locations")]
     public sealed class LocationsController : ControllerBase
     {
         private readonly ILocationService _locationService;
