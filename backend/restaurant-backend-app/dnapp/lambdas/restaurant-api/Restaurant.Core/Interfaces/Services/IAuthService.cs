d5685a0354c8b07614af96b2bdf767a007d8989f
pidarinka <daryna.suprun@nure.ua>
2026-03-04 09:43:57 +0100

    expose IAuthService.IsWaiter for role checks
    olve conflicts

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/Services/IAuthService.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/Services/IAuthService.cs
@@ -1,4 +1,9 @@
+<<<<<<< HEAD:backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/Services/IAuthService.cs
 ﻿using Restaurant.Core.SharedModels;
+=======
+﻿using System.Security.Claims;
+using Restaurant.Core.Models;
+>>>>>>> 7a195bb (expose IAuthService.IsWaiter for role checks):backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/IAuthService.cs
 
 namespace Restaurant.Core.Interfaces.Services;
 
@@ -6,5 +11,6 @@ public interface IAuthService
 {
     Task SignUpAsync(string email, string password, string firstName, string lastName);
     Task<AuthResult> SignInAsync(string email, string password);
-    
+
+    bool IsWaiter(ClaimsPrincipal user);
 }
