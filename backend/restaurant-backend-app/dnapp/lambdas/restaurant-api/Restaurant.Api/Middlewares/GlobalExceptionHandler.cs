267010ef724e92319371d4f03b992e73e8f2b005
pidarinka <daryna.suprun@nure.ua>
2026-03-06 17:50:54 +0100

    test scaffolding + CI

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Api/Middlewares/GlobalExceptionHandler.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Api/Middlewares/GlobalExceptionHandler.cs
@@ -22,6 +22,7 @@ public class GlobalExceptionHandler : IExceptionHandler
             UserAlreadyExistsException ex => (StatusCodes.Status409Conflict, ex.Message),
             InvalidCredentialsException ex => (StatusCodes.Status401Unauthorized, ex.Message),
             AuthException ex => (StatusCodes.Status400BadRequest, ex.Message),
+            UnauthorizedAccessException ex => (StatusCodes.Status403Forbidden, ex.Message),
 
             _ => (StatusCodes.Status500InternalServerError, exception.Message)
         };
