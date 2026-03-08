db2ea299ee05f4c590fe0b37ced83a297a221052
Kirikosha <kirikosha@gmail.com>
2026-03-08 11:31:11 +0100

    (feature): add UpdateReservationDto

---
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/DTOs/UpdateReservationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Core.DTOs;
+
+public class UpdateReservationDTO
+{
+    public string Id { get; set; }
+    public int GuestNumber { get; set; }
+    public string TableNumber { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+}
