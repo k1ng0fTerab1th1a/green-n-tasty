ece391e510496a0cf8b7103498f7900769cb645f
Kirikosha <kirikosha@gmail.com>
2026-03-23 11:15:51 +0100

    (fix): removed unused method

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Services/S3FileService.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Services/S3FileService.cs
@@ -7,21 +7,6 @@ public class S3FileService(IAmazonS3 _s3)
 {
     private const string BucketName = "run20-team2-project-education-artifacts-dev";
     
-    public async Task<string> GetFileContentAsync(string key, CancellationToken ct = default)
-    {
-        // key example: "restaurant-menu/menu.pdf"
-        var request = new GetObjectRequest
-        {
-            BucketName = BucketName,
-            Key        = key
-        };
-
-        using var response = await _s3.GetObjectAsync(request, ct);
-        using var reader   = new StreamReader(response.ResponseStream);
-
-        return await reader.ReadToEndAsync(ct);
-    }
-    
     public async Task<Stream> GetFileStreamAsync(string key, CancellationToken ct = default)
     {
         var response = await _s3.GetObjectAsync(
