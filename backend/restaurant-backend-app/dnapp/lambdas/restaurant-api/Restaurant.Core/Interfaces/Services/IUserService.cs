20fa85ead6ad148c532dc1c0810318e2d6fdcba6
Kirikosha <kirikoshamain@gmail.com>
2026-03-30 19:31:34 +0200

    (feature): add user update

--- backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/Services/IUserService.cs
+++ backend/restaurant-backend-app/dnapp/lambdas/restaurant-api/Restaurant.Core/Interfaces/Services/IUserService.cs
@@ -5,4 +5,5 @@ namespace Restaurant.Core.Interfaces.Services;
 public interface IUserService
 {
     Task<Result> UpdateEmailAsync(string userId, string newEmail, CancellationToken ct);
+    Task<Result> UpdateUserNameAsync(string userId, string firstName, string lastName, CancellationToken ct);
 }
